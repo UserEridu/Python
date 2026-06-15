bf6378adcc2389a58341b038ce2c0584870150ff
UserEridu <mr.daniel.8.1.17@gmail.com>
2026-06-15 17:23:15 -0400

    Actualización - UserEridu

--- adivina.py
+++ adivina.py
@@ -2,13 +2,14 @@
 Juego de adivina el número
 '''
 import paquete.utilidades as util
+from random import randint
 
 def main() -> None:
     util.encabezado('Juego de adivina el número', 46)
     alias: str = util.leer_texto('ingresa tu alias', 'alias')
     util.linea
     #print(f'Alias: {alias}')
-    secreto: int = 17
+    secreto: int = randint(1, 20)
     número: int = 0
     
     jugar(secreto, número, alias)
@@ -16,8 +17,18 @@ def main() -> None:
 def jugar(secreto: int, numero: int, alias: str) -> None:
     alias = alias.upper()
     msj: str = ''
+    intentos: int = 1
     while numero  != secreto:
+        if intentos == 4:
+            msj = 'perdiste el número secreto era el'
+            util.linea(46)
+            print(f'{alias} {msj} {secreto}')
+            util.linea(46)
+            break
+        
         numero = util.leer_número_entero('ingresa tu número')
+        intentos += 1
+        
         if numero > secreto:
             msj: str = 'Tu número es mayor que el secreto'
             print(f'{alias}, {msj}')
